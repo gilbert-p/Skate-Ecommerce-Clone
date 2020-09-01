6591783a854c0b9f2845c89fbfff31089f4f99c5
epoxi117 <gplacid2@calstatela.edu>
2020-09-01 08:16:09 -0700

    Updates

---
+++ precache-manifest.d876d3cd60018a736ac5a67039f08b9b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe37fc083f8d91c9cae6c77bb64a9dac",
+    "url": "/Skate-Ecommerce-Clone/index.html"
+  },
+  {
+    "revision": "34812391dc0debe664ea",
+    "url": "/Skate-Ecommerce-Clone/static/css/main.d1414a6c.chunk.css"
+  },
+  {
+    "revision": "992d6232df60a95803dd",
+    "url": "/Skate-Ecommerce-Clone/static/js/2.610d7dcd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Skate-Ecommerce-Clone/static/js/2.610d7dcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34812391dc0debe664ea",
+    "url": "/Skate-Ecommerce-Clone/static/js/main.a3649adb.chunk.js"
+  },
+  {
+    "revision": "2987abf6428844ace7fb",
+    "url": "/Skate-Ecommerce-Clone/static/js/runtime-main.7bd14fc0.js"
+  },
+  {
+    "revision": "e26532f2abdf4943bc544a3d66c3d027",
+    "url": "/Skate-Ecommerce-Clone/static/media/ext_img_plugs_accessories_1.e26532f2.jpg"
+  },
+  {
+    "revision": "61fc230fee87c84419cc44005de6c285",
+    "url": "/Skate-Ecommerce-Clone/static/media/ext_img_plugs_clothing_1.61fc230f.jpg"
+  },
+  {
+    "revision": "76f5876b879cd90e17bc6fe26a99b2fe",
+    "url": "/Skate-Ecommerce-Clone/static/media/ext_img_plugs_decks_4.76f5876b.jpg"
+  },
+  {
+    "revision": "bafbccf569096fa5f11b14eba0777754",
+    "url": "/Skate-Ecommerce-Clone/static/media/ext_img_plugs_hats_3.bafbccf5.jpg"
+  },
+  {
+    "revision": "fa205e6c222c83ad7d5e587c467ea661",
+    "url": "/Skate-Ecommerce-Clone/static/media/ext_img_plugs_modernica_1.fa205e6c.jpg"
+  },
+  {
+    "revision": "439e3a08c7552edf13aefbd84d75c378",
+    "url": "/Skate-Ecommerce-Clone/static/media/ext_img_plugs_pop_secret_1.439e3a08.jpg"
+  },
+  {
+    "revision": "8be462311888cb4dc6e757e0f35c093c",
+    "url": "/Skate-Ecommerce-Clone/static/media/ext_img_plugs_reflective_1.8be46231.jpg"
+  },
+  {
+    "revision": "565e188c708fa1624a98e71607d6f42e",
+    "url": "/Skate-Ecommerce-Clone/static/media/ext_img_plugs_skull_of_fame_3.565e188c.jpg"
+  },
+  {
+    "revision": "e20dbde62bd6235b5f43bfca05a05bb3",
+    "url": "/Skate-Ecommerce-Clone/static/media/slideshow_img_1.e20dbde6.jpg"
+  }
+]);
